cc64f8c354f04cd57c3a3f5471b4931edc5ed14b
Denis Matakov <denis@matakov.com>
2021-03-05 08:47:19 +0300

    Index page template

---
+++ recipe/views.py
@@ -0,0 +1,26 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+def index(request):
+    # post_list = Post.objects.select_related(
+    #     "author", "group"
+    # ).order_by("-pub_date").all()
+    #
+    # paginator = Paginator(post_list, 10)
+    # # показывать по 10 записей на странице.
+    # page_number = request.GET.get("page")
+    # # переменная в URL с номером запрошенной страницы
+    # page = paginator.get_page(page_number)
+    # # получить записи с нужным смещением
+    return render(
+        request,
+        "index.html",
+        {
+            # "page": page,
+            # "paginator": paginator
+        }
+    )
