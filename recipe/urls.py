51cfcaf5ae63af92033874f402f3961c06bd6d0f
Denis Matakov <denis@matakov.com>
2021-03-06 11:23:27 +0300

    Single recipe template

--- recipe/urls.py
+++ recipe/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("new-recipe/", views.new_recipe, name="new_recipe"),
     path("edit-recipe/", views.edit_recipe, name="edit_recipe"),
     path("shoplist/", views.shoplist, name="shoplist"),
+    path("recipe/", views.single, name="single"),
 ]
