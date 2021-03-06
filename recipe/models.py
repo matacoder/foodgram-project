8d6280c80bf7246b0d300bab5531eec38e1a1297
Denis Matakov <denis@matakov.com>
2021-03-06 19:49:32 +0300

    Models project

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+"""
+
+Ingredient
+- name
+- measure
+
+Recipe
+- name
+- user id
+- tags ManyToMany
+- description
+- cook_time
+- photo
+- slug
+
+RecipeIngredients
+- id recipe
+- id ingredient
+- numeric measure
+
+Tags
+- name
+
+RecipeTags
+- id recipe
+- id tags
+
+
+Following
+- id user
+- id user
+
+Favorite
+- id user
+- id recipe
+
+List
+- id user
+- id recipe
+
+"""
