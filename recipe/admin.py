5dba74c472d6f5b6a04f46d81d525443065545b5
Denis Matakov <denis@matakov.com>
2021-03-07 12:54:07 +0300

    Ingredient Model (inc. migrations and admin repr.)

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipe.models import Ingredient
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Ingredient, IngredientAdmin)
--- recipe/models.py
+++ recipe/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    """ Ingredient model """
+
+    name = models.CharField(max_length=255)
+    measure = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'Ingredient: {self.name}'
+
+
 """
 
 AbstractUser
