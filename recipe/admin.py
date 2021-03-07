97f0c42a7366e4b8d2e1f3c2b72c69674f740658
Denis Matakov <denis@matakov.com>
2021-03-07 13:30:44 +0300

    Amount, Tag, Recipe (inc. migrations and admin repr.)

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
 
-from recipe.models import Ingredient
+from recipe.models import Ingredient, Tag, Amount, Recipe
 
 
 class IngredientAdmin(admin.ModelAdmin):
     pass
 
 
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+class AmountAdmin(admin.ModelAdmin):
+    pass
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Amount, AmountAdmin)
+admin.site.register(Recipe, RecipeAdmin)
--- recipe/models.py
+++ recipe/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class Ingredient(models.Model):
     """ Ingredient model """
@@ -8,10 +10,45 @@ class Ingredient(models.Model):
     measure = models.CharField(max_length=30)
 
     def __str__(self):
-        return f'Ingredient: {self.name}'
+        return f'Ingredient: {self.name} ({self.measure})'
+
+
+class Tag(models.Model):
+    """ Breakfast, Lunch, Dinner default tags here """
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'Tag: {self.name}'
+
+
+class Recipe(models.Model):
+    """ Recipe model """
+
+    name = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="recipes")
+    tags = models.ManyToManyField(Tag, related_name="recipes")
+    ingredients = models.ManyToManyField(Ingredient, through="Amount", related_name="recipes")
+    description = models.TextField(max_length=1000)
+    cook_time = models.IntegerField()
+    image = models.ImageField(upload_to="recipes/", blank=True, null=True)
+    slug = models.SlugField(unique=True, max_length=200)
+    favorite = models.ManyToManyField(User, blank=True, related_name="favorite_recipes")
+    listed = models.ManyToManyField(User, blank=True, related_name="listed_recipes")
+
+    def __str__(self):
+        return f'Recipe: {self.name} by {self.author}'
+
+
+class Amount(models.Model):
+    """ Support model for Ingredient&Recipe ManyToMany relation """
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="amounts")
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="amounts")
+    amount = models.IntegerField()
 
 
 """
+Database Project
+
 
 AbstractUser
 - following Many2Many AbstractUser
