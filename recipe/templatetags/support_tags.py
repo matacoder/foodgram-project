4c84790d542eaa4fbd2ef2bfae164b9742ab31f0
Denis Matakov <denis@matakov.com>
2021-03-09 22:57:07 +0300

    is_following template tag

--- recipe/templatetags/support_tags.py
+++ recipe/templatetags/support_tags.py
@@ -14,6 +14,7 @@ def how_many_listed(user):
 def how_many_favorites(user):
     return user.favorite_recipes.count()
 
+
 @register.simple_tag
 def how_many_following(user):
     return user.following.count()
@@ -27,3 +28,8 @@ def is_favorite(recipe, user):
 @register.simple_tag
 def is_listed(recipe, user):
     return recipe.listed.filter(id=user.id).exists()
+
+
+@register.simple_tag
+def is_following(user_id, user):
+    return user.following.filter(id=user_id).exists()
