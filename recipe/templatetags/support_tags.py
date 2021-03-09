060f8ce4cd28522551f0078a19f82157792f7148
Denis Matakov <denis@matakov.com>
2021-03-09 22:45:36 +0300

    Following, Favorites counters, is_favorite, is_listed template tags

--- recipe/templatetags/support_tags.py
+++ recipe/templatetags/support_tags.py
@@ -1,5 +1,7 @@
 from django import template
 
+from recipe.models import Recipe
+
 register = template.Library()
 
 
@@ -11,3 +13,17 @@ def how_many_listed(user):
 @register.simple_tag
 def how_many_favorites(user):
     return user.favorite_recipes.count()
+
+@register.simple_tag
+def how_many_following(user):
+    return user.following.count()
+
+
+@register.simple_tag
+def is_favorite(recipe, user):
+    return recipe.favorite.filter(id=user.id).exists()
+
+
+@register.simple_tag
+def is_listed(recipe, user):
+    return recipe.listed.filter(id=user.id).exists()
