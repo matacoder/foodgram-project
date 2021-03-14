c25b6ba4fac1eb4ce1fcd156bd25b6d10a181b1c
Denis Matakov <denis@matakov.com>
2021-03-14 22:08:43 +0300

    isort, flake8

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import handler404, handler500
 from django.contrib import admin
 from django.urls import include, path
-
-from django.conf.urls import handler404, handler500
 
 handler404 = "recipe.views.page_not_found"  # noqa
 handler500 = "recipe.views.server_error"  # noqa
--- recipe/urls.py
+++ recipe/urls.py
@@ -1,7 +1,4 @@
-from urllib import request
-
 from django.urls import path
-from django.views import defaults
 
 from . import views
 
