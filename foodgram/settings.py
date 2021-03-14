7f95866f1cd9e361407a256ac9bb865bd443ef91
Denis Matakov <denis@matakov.com>
2021-03-14 20:25:13 +0300

    django-eml-email-backend backend email

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -102,7 +102,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+EMAIL_BACKEND = 'eml_email_backend.EmailBackend'
+EMAIL_FILE_PATH = 'email/'
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
