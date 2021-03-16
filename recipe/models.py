e1f3d94de698535bc04b57df159000b2437346b9
Denis Matakov <denis@matakov.com>
2021-03-16 21:56:50 +0300

    Recipe, Ingredient, Tag models verbose_names

--- recipe/models.py
+++ recipe/models.py
@@ -8,8 +8,8 @@ from users.models import User
 class Ingredient(models.Model):
     """ Ingredient model """
 
-    name = models.CharField(max_length=255)
-    measure = models.CharField(max_length=30)
+    name = models.CharField(max_length=255, verbose_name="Ingredient name")
+    measure = models.CharField(max_length=30, verbose_name="Measure")
 
     def __str__(self):
         return f'Ingredient: {self.name} ({self.measure})'
@@ -17,9 +17,11 @@ class Ingredient(models.Model):
 
 class Tag(models.Model):
     """ Breakfast, Lunch, Dinner default tags here """
-    name = models.CharField(max_length=255)
-    color = models.CharField(max_length=100, blank=True)
-    nice_name = models.CharField(max_length=255, blank=True)
+    name = models.CharField(max_length=255, verbose_name="Tag's name")
+    color = models.CharField(max_length=100, blank=True,
+                             verbose_name="Tag's color")
+    nice_name = models.CharField(max_length=255, blank=True,
+                                 verbose_name="Nice english name for tag")
 
     def __str__(self):
         return f'Tag: {self.name}'
@@ -28,32 +30,38 @@ class Tag(models.Model):
 class Recipe(models.Model):
     """ Recipe model """
 
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name="Recipe's title")
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name="recipes")
-    tags = models.ManyToManyField(Tag, related_name="recipes")
+                               related_name="recipes",
+                               verbose_name="Recipe's author")
+    tags = models.ManyToManyField(Tag, related_name="recipes",
+                                  verbose_name="Recipe's tags")
     ingredients = models.ManyToManyField(
-        Ingredient, through="Amount", related_name="recipes"
+        Ingredient, through="Amount", related_name="recipes",
+        verbose_name="Recipe's ingredients"
     )
-    description = models.TextField(max_length=1000)
-    cook_time = models.IntegerField()
-    image = models.ImageField(upload_to="recipes/", blank=True)
+    description = models.TextField(max_length=1000, verbose_name="Description")
+    cook_time = models.PositiveSmallIntegerField(verbose_name="Cooking time")
+    image = models.ImageField(upload_to="recipes/", blank=True,
+                              verbose_name="Recipe's Image")
     slug = AutoSlugField(populate_from="name", allow_unicode=True, unique=True,
-                         editable=True)
+                         editable=True, verbose_name="Slug")
     favorite = models.ManyToManyField(User, blank=True,
-                                      related_name="favorite_recipes")
+                                      related_name="favorite_recipes",
+                                      verbose_name="Who added to favorites")
     listed = models.ManyToManyField(User, blank=True,
-                                    related_name="listed_recipes")
+                                    related_name="listed_recipes",
+                                    verbose_name="Who added to cart")
     pub_date = models.DateTimeField(
-        verbose_name="Дата публикации",
+        verbose_name="Publication date",
         auto_now_add=True,
         db_index=True
     )
 
     class Meta:
         ordering = ('-pub_date',)
-        verbose_name = 'рецепт'
-        verbose_name_plural = 'рецепты'
+        verbose_name = 'recipe'
+        verbose_name_plural = 'recipes'
 
     def __str__(self):
         return f'Recipe: {self.name} by {self.author}'
