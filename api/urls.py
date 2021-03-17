bf1b4d6b17d7482bd96de2543ab04d229ccdd297
Denis Matakov <denis@matakov.com>
2021-03-17 09:10:24 +0300

    Exclude version of api

--- api/urls.py
+++ api/urls.py
@@ -1,18 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 # API for JS front-end Version 1.0
 urlpatterns = [
-    path("v1/purchases/", views.purchases, name="purchases"),  # API
-    path("v1/purchases/<int:recipe_id>/", views.purchases_remove,
-         name="purchases_remove"),  # API
-    path("v1/subscriptions/", views.subscriptions, name="subscriptions"),
-    # API
-    path("v1/subscriptions/<int:user_id>/", views.subscriptions_remove,
-         name="subscriptions"),  # API
-    path("v1/favorites/", views.favorites, name="favorites"),  # API
-    path("v1/favorites/<int:recipe_id>/", views.favorites_remove,
-         name="favorites_remove"),  # API
-    path("v1/ingredients/", views.ingredients, name="ingredients"),  # API
+    path("v1/", include([
+        path("purchases/", views.purchases, name="purchases"),  # API
+        path("purchases/<int:recipe_id>/", views.purchases_remove,
+             name="purchases_remove"),  # API
+        path("subscriptions/", views.subscriptions, name="subscriptions"),
+        # API
+        path("subscriptions/<int:user_id>/", views.subscriptions_remove,
+             name="subscriptions"),  # API
+        path("favorites/", views.favorites, name="favorites"),  # API
+        path("favorites/<int:recipe_id>/", views.favorites_remove,
+             name="favorites_remove"),  # API
+        path("ingredients/", views.ingredients, name="ingredients"),  # API
+    ]))
 ]
