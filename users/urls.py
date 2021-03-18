4500c0e9a5b64c200d040123a4c0ec660b092ab1
Denis Matakov <denis@matakov.com>
2021-03-18 11:43:37 +0300

    Password reset form updated

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import PasswordResetDoneView
 from django.urls import include, path
 
 from . import views
@@ -6,4 +7,7 @@ urlpatterns = [
     path("signup/", views.SignUp.as_view(), name="signup"),
     path("profile/", views.profile, name="profile"),
     path("", include("django.contrib.auth.urls")),
+    path("reset/done/", PasswordResetDoneView.as_view(
+        template_name="registration/password_reset_done.html"),
+         name="password_reset_done"),
 ]
