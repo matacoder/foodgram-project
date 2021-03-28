7bae750824438348d70dc7e0dfc6ebff5887be60
Denis Matakov <denis@matakov.com>
2021-03-28 22:09:14 +0300

    Following counters works

--- static/custom.js
+++ static/custom.js
@@ -3,14 +3,11 @@ counterFav = document.querySelector("#counter_fav")
 const starElements = document.querySelectorAll('[name="favorites"]')
 
 
-
 for (let starElement of starElements) {
     starElement.addEventListener('click', event => {
         if (starElement.dataset.out) {
-            console.dir(starElement.dataset.out)
             ++counterFav.innerHTML
         } else {
-            console.dir(starElement.dataset.out)
             --counterFav.innerHTML
         }
     })
@@ -19,13 +16,12 @@ for (let starElement of starElements) {
 counterFol = document.querySelector("#counter_fol")
 const subscribeElements = document.querySelectorAll('[name="subscribe"]')
 
+
 for (let subscribeElement of subscribeElements) {
     subscribeElement.addEventListener('click', event => {
         if (subscribeElement.dataset.out) {
-            console.dir(subscribeElement.dataset.out)
             ++counterFol.innerHTML
         } else {
-            console.dir(subscribeElement.dataset.out)
             --counterFol.innerHTML
         }
     })
