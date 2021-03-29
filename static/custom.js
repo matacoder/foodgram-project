fe368ad44a89e01dad369053a3718eca603aa551
Denis Matakov <denis@matakov.com>
2021-03-29 14:32:04 +0300

    Set Listeners JavaScript

--- static/custom.js
+++ static/custom.js
@@ -1,28 +1,16 @@
 counterFav = document.querySelector("#counter_fav")
-
 const starElements = document.querySelectorAll('[name="favorites"]')
-
-
-for (let starElement of starElements) {
-    starElement.addEventListener('click', event => {
-        if (starElement.dataset.out) {
-            ++counterFav.innerHTML
-        } else {
-            --counterFav.innerHTML
-        }
-    })
-}
 
 counterFol = document.querySelector("#counter_fol")
 const subscribeElements = document.querySelectorAll('[name="subscribe"]')
 
-
-for (let subscribeElement of subscribeElements) {
-    subscribeElement.addEventListener('click', event => {
-        if (subscribeElement.dataset.out) {
-            ++counterFol.innerHTML
-        } else {
-            --counterFol.innerHTML
-        }
-    })
+const setListeners = (elements, counter) => {
+    for (let element of elements) {
+        element.addEventListener('click', event => {
+            let delta = element.dataset.out ? 1 : -1
+            counter.innerHTML = parseInt(counter.innerHTML) + delta
+        })
+    }
 }
+setListeners(subscribeElements, counterFol)
+setListeners(starElements, counterFav)
