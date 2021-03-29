2f0a36ad17121c809bae4c526153e7fd5b7a57d1
Denis Matakov <denis@matakov.com>
2021-03-29 21:22:29 +0300

    Set Listeners JavaScript comments

--- static/custom.js
+++ static/custom.js
@@ -1,12 +1,16 @@
+// Looking for actual counter number on page
 counterFav = document.querySelector("#counter_fav")
+// Get array of buttons 'Add to favorites' (star icon)
 const starElements = document.querySelectorAll('[name="favorites"]')
 
 counterFol = document.querySelector("#counter_fol")
 const subscribeElements = document.querySelectorAll('[name="subscribe"]')
 
+// Function to add 'on click' action (listener) on every element of array
 const setListeners = (elements, counter) => {
     for (let element of elements) {
         element.addEventListener('click', event => {
+            // check if attribute data-out === 'true' and act accordingly
             let delta = element.dataset.out ? 1 : -1
             counter.innerHTML = parseInt(counter.innerHTML) + delta
         })
